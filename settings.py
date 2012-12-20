6bda143820177290a6c3eadd01a6894ba127608b
Max Peterson <computmaxer@gmail.com>
2012-12-19 23:25:40 -0600

    Adding settings local.py

--- settings.py
+++ settings.py
@@ -4,9 +4,14 @@ FACEBOOK_APP_SECRET = 'e1490a8ce6200c7ff938d7f273087fbc'
 GOOGLE_CLIENT_ID = '593910760953.apps.googleusercontent.com'
 GOOGLE_SECRET = 'F2ngZOaKzAgUbhe3xVUqgYYj'
 
-DEBUG=True
-SECRET_KEY='dev_key_h8hfne89vm'
-CSRF_ENABLED=True
-CSRF_SESSION_LKEY='dev_key_h8asSNJ9s9=+'
+DEBUG = False
+SECRET_KEY = 'dev_key_h8hfne89vm'
+CSRF_ENABLED = True
+CSRF_SESSION_LKEY = 'dev_key_h8asSNJ9s9=+'
 
-MEDIA_MERGED = False
+MEDIA_MERGED = True
+
+try:
+    from settingslocal import *
+except ImportError:
+    pass
