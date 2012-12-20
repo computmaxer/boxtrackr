f76f0cabb196a6daf74ef434064f39d147aef635
Max Peterson <computmaxer@gmail.com>
2012-12-19 23:08:19 -0600

    Beginning shipping api.

---
+++ shipping/api.py
@@ -0,0 +1,13 @@
+#TODO: Create a flask extension/library out of this?
+from flask.templating import render_template
+
+import requests
+
+
+def _get_ups_tracking_xml(tracking_number):
+    path = "https://wwwcie.ups.com/ups.app/xml/Track"
+    ctx = {'tracking_number':tracking_number}
+    xml_req = render_template("xml/ups_tracking_request.html", **ctx)
+
+    r = requests.post(path, data=xml_req)
+    return r.text
