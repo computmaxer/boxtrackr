b08ac6ed303bd17b0621d9ff8a19e857544d7f85
Max Peterson <computmaxer@gmail.com>
2012-12-21 17:15:28 -0600

    API work in the models.

--- shipping/api.py
+++ shipping/api.py
@@ -8,6 +8,55 @@ import settings
 import logging
 
 
+NO_INFO = -1
+RECEIVED_NOTICE = 0
+IN_TRANSIT = 1
+OUT_FOR_DELIVERY = 2
+DELIVERED = 3
+
+STATUS_LIST = [(NO_INFO, "Carrier has no information"),
+               (RECEIVED_NOTICE, "Carrier has been notified of the shipment"),
+               (IN_TRANSIT, "In Transit"),
+               (OUT_FOR_DELIVERY, "Out for Delivery"),
+               (DELIVERED, "Delivered")
+]
+STATUS_DICT = {
+    NO_INFO: "Carrier has no information",
+    RECEIVED_NOTICE: "Carrier has been notified of the shipment",
+    IN_TRANSIT: "In Transit",
+    OUT_FOR_DELIVERY: "Out for Delivery",
+    DELIVERED: "Delivered",
+}
+
+
+def determine_carrier(tracking_number):
+    #TODO: regex matching to return the proper carrier.
+    #return 'fedex'
+    #return 'usps'
+    return 'ups'  # TODO: Temporary, obviously.
+
+
+def query_tracking(tracking_number):
+    carrier = determine_carrier(tracking_number)
+    method = locals["query_%s_tracking" % carrier]
+    return method(tracking_number)
+
+
+def _determine_status(status_text):
+    low_text = status_text.lower()
+    #TODO: Finish this.
+    if 'no' in low_text:  # TODO: figure out what this needs to be
+        return NO_INFO
+    elif 'received' in low_text:  # TODO: figure out what this needs to be
+        return RECEIVED_NOTICE
+    elif 'transit' in low_text:  # TODO: figure out what this needs to be
+        return IN_TRANSIT
+    elif 'out' in low_text:  # TODO: figure out what this needs to be
+        return OUT_FOR_DELIVERY
+    elif 'delivered' in low_text:  # TODO: figure out what this needs to be
+        return DELIVERED
+
+
 # UPS Tracking API
 ####################################################################################################
 def query_ups_tracking(tracking_number):
@@ -31,10 +80,10 @@ def _parse_ups_tracking_response_xml(root):
             packages = []
             for package in shipment.findall('Package'):
                 activity = package.find('Activity')
-                status = activity.find('Status').find('StatusType').find('Description').text
+                status_text = activity.find('Status').find('StatusType').find('Description').text
                 info = {
                     'tracking_number': package.find('TrackingNumber').text,
-                    'status': status,
+                    'status': _determine_status(status_text),
                 }
                 #TODO: Add more info to the dict?
                 packages.append(info)
@@ -51,7 +100,7 @@ def _get_ups_tracking_xml(tracking_number):
     ctx = {
         'tracking_number': tracking_number,
         'password': settings.UPS_API_PASSWORD,
-        }
+    }
     xml_req = render_template("xml/ups_tracking_request.html", **ctx)
 
     r = requests.post(path, data=xml_req)
@@ -114,7 +163,7 @@ def _get_usps_tracking_xml(tracking_number):
     ctx = {
         'tracking_number': tracking_number,
         'password': settings.USPS_API_PASSWORD,
-        }
+    }
     xml_req = render_template("xml/usps_tracking_request.html", **ctx)
 
     r = requests.post(path, data=xml_req)
