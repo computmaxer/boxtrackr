1949be7ac886423316181086991fba5ff9f9cdf0
Michael Davis <mike.philip.davis@gmail.com>
2012-12-24 10:13:57 -0600

    More work on emails.
    Making a placeholder user
    parsing the from address correct.

--- mail/utils.py
+++ mail/utils.py
@@ -5,11 +5,11 @@ def check_message_for_tracking_number(message):
 
     #TODO Get an accurate list of regexes to use
     _REGEX = {
-        'ups': re.compile(r"(1Z?[0-9A-Z]{3}?[0-9A-Z]{3}?[0-9A-Z]{2}?[0-9A-Z]{4}?[0-9A-Z]{3}?[0-9A-Z]|[\dT]\d\d\d?\d\d\d\d?\d\d\d)"),
-        'usps': re.compile(r'(E\D{1}\d{9}\D{2}$|9\d{15,21})'),
+        re.compile(r"(1Z?[0-9A-Z]{3}?[0-9A-Z]{3}?[0-9A-Z]{2}?[0-9A-Z]{4}?[0-9A-Z]{3}?[0-9A-Z]|[\dT]\d\d\d?\d\d\d\d?\d\d\d)"): 'ups',
+        re.compile(r'(E\D{1}\d{9}\D{2}$|9\d{15,21})'): 'usps'
     }
 
-    for type, regex in _REGEX.items():
+    for regex, type in _REGEX.items():
         m = re.search(regex, message)
         if m:
             #TODO This only gets the first tracking number found.  We should look into possibly
