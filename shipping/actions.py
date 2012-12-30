1c4cc6ff498a348e25eef565e66d196da4031760
Max Peterson <computmaxer@gmail.com>
2012-12-29 19:04:05 -0600

    Can now edit and remove packages.

--- shipping/actions.py
+++ shipping/actions.py
@@ -2,6 +2,8 @@ from google.appengine.ext import db
 
 from shipping import models as shipping_models
 
+from datetime import datetime, timedelta
+
 
 def create_package(data, user):
     p = shipping_models.Package(**data)
@@ -17,3 +19,29 @@ def get_user_packages(user):
         setattr(package, 'STATUS_TEXT', package.get_status_text())
         packages.append(package)
     return packages
+
+
+def get_package(keystr=None, key=None):
+    if keystr:
+        return shipping_models.Package.get(db.Key(keystr))
+    elif key:
+        return shipping_models.Package.get(key)
+
+
+def edit_package(package, data):
+    package.name = data.get('name')
+    package.tracking_number = data.get('tracking_number')
+    package.site = data.get('site')
+    package.description = data.get('description')
+
+    # Set the last_checked time so that it will refresh from the api on next load
+    new_time = datetime.now() - timedelta(minutes=shipping_models.Package.UPDATE_INTERVAL_MINUTES)
+    package.last_checked = new_time
+    package.put()
+
+
+def delete_package(package, user):
+    if package.owner.key() == user.key():
+        package.delete()
+        return True
+    return False
