f7fe1a263ff2d6167678b2c29ff13d4225744343
Michael Davis <mike.philip.davis@gmail.com>
2013-01-05 20:22:38 -0600

    Changing email author to support@boxtrackr.com

--- base/mail.py
+++ base/mail.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import render_template
 
 from google.appengine.api import mail
@@ -5,11 +7,10 @@ from google.appengine.ext import deferred
 
 
 def send_email(email, subject, body):
-    import logging
-    logging.warning(body)
+    logging.debug(body)
 
     #TODO: Get @boxtrackr.com email set up and use one of those emails
-    sender="Michael Davis <mike.philip.davis@gmail.com>"
+    sender="Boxtrackr Support <support@boxtrackr.com>"
     deferred.defer(mail.send_mail, sender=sender, to=email, subject=subject, body=body)
 
 
@@ -20,4 +21,3 @@ def generate_email_body(template, context=None, **kwargs):
     ctx.update(kwargs)
 
     return render_template(template, **ctx)
-
